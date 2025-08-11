a6dd79cdf93887552e67d241f3f8b42d5e1333e8
xxschi <141630459+xxschi@users.noreply.github.com>
2025-08-11 16:14:28 +0900

    feat: 마메치 페이지 업로드

---
+++ vite-project/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/my-mamechi/', // 여기에 실제 저장소 이름 입력
+  build: {
+    outDir: 'dist',
+  },
+});
